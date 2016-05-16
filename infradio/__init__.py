d3b48dfc8deb6ed40a1461e844eadfee017bd3a0
John Salubrious <johnsalubrious@mailinator.com>
2016-05-16 15:50:57 -0400

    BUMP v1.0.0-pre

--- infradio/__init__.py
+++ infradio/__init__.py
@@ -1,1 +1,20 @@
+
+import os
+import json
+
 from .api import *  # noqa
+
+__version__ = "1.0.0-pre"
+
+#
+# Find any Git info the build system left for us
+#
+
+_git_info_filename = os.path.join(os.path.dirname(__file__),
+                                  '__git__.json')
+
+if os.path.exists(_git_info_filename):
+    with open(_git_info_filename) as git_info_file:
+        __git__ = json.load(git_info_file)
+else:
+    __git__ = None
